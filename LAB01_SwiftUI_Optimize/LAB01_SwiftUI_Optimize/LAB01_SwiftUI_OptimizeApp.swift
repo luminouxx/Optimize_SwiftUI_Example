8f3a94768841c699045b5c7d004d9c835f2de612
YoungJin Jeong <81943525+luminouxx@users.noreply.github.com>
2026-01-07 17:15:01 +0900

    chore: init project

---
+++ LAB01_SwiftUI_Optimize/LAB01_SwiftUI_Optimize/LAB01_SwiftUI_OptimizeApp.swift
@@ -0,0 +1,17 @@
+//
+//  LAB01_SwiftUI_OptimizeApp.swift
+//  LAB01_SwiftUI_Optimize
+//
+//  Created by luminoux on 1/7/26.
+//
+
+import SwiftUI
+
+@main
+struct LAB01_SwiftUI_OptimizeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
